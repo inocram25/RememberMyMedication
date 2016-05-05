620fee8908f7245b7980bf2b2d5eedc05093e953
guilherme babugia <babugia@icloud.com>
2016-05-05 14:38:00 -0300

    picker working

--- RememberMedication/RememberMedication/AddAppointimentsTableViewController.swift
+++ RememberMedication/RememberMedication/AddAppointimentsTableViewController.swift
@@ -12,14 +12,18 @@ class AddAppointimentsTableViewController: UITableViewController {
 
     let dateFormatter = NSDateFormatter()
     var pickerVisible = false
+    var alarmPickerVisible = false
     var dateAlarm = [[String]]()
     
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var alarmLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var doctorTextfield: UITextField!
     @IBOutlet weak var notesTextfield: UITextField!
     @IBOutlet weak var localTextfield: UITextField!
     @IBOutlet weak var alarmPickerView: UIPickerView!
+    
+    var alarm = (day:"1 day", hour:"0 hours", min:"0 mins")
     
     //pensar em como configurar o local e o notes
     
@@ -37,21 +41,21 @@ class AddAppointimentsTableViewController: UITableViewController {
         
         var hours = [String]()
         for i in 1...14 {
-            hours.append(String(i) + "days")
+            hours.append(String(i) + " days")
         }
         
         dateAlarm.append(hours)
         
         hours.removeAll()
         for i in 0...23 {
-            hours.append(String(i) + "hours")
+            hours.append(String(i) + " hours")
         }
         
         dateAlarm.append(hours)
         
         hours.removeAll()
         for i in 0...59 {
-            hours.append(String(i) + "mins")
+            hours.append(String(i) + " mins")
         }
         
         dateAlarm.append(hours)
@@ -59,11 +63,15 @@ class AddAppointimentsTableViewController: UITableViewController {
         alarmPickerView.dataSource = self
         alarmPickerView.delegate = self
         
+        alarmLabel.text = "1 day before."
+        
     }
     
     @IBAction func pickerDate(sender: UIDatePicker) {
         dateLabel.text = dateFormatter.stringFromDate(sender.date)
     }
+    
+    
     @IBAction func saveButtonFunction(sender: AnyObject) {
         
         guard nameTextField.text != nil else { return }
@@ -80,6 +88,11 @@ class AddAppointimentsTableViewController: UITableViewController {
             pickerVisible = !pickerVisible
             tableView.reloadData()
         }
+        if indexPath.row == 3 && indexPath.section == 0 {
+            alarmPickerVisible = !alarmPickerVisible
+            tableView.reloadData()
+        }
+        
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
@@ -88,6 +101,11 @@ class AddAppointimentsTableViewController: UITableViewController {
         if indexPath.row == 2 && indexPath.section == 0 {
             return pickerVisible == false ? 0.0 : 165.0
         }
+        
+        if indexPath.row == 4 && indexPath.section == 0 {
+            return alarmPickerVisible == false ? 0.0 : 165.0
+        }
+
         return 44.0
     }
     
@@ -105,10 +123,26 @@ extension AddAppointimentsTableViewController: UIPickerViewDelegate, UIPickerVie
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return dateAlarm.count
+        return dateAlarm[component].count
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return dateAlarm[component][row]
     }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+        if component == 0 {
+            alarm.day = dateAlarm[component][row]
+        }
+        else if component == 1 {
+            alarm.hour = dateAlarm[component][row]
+        }
+        else {
+            alarm.min = dateAlarm[component][row]
+        }
+        
+        alarmLabel.text = alarm.day + ", " + alarm.hour + " and " + alarm.min + " before."
+    }
+    
 }
