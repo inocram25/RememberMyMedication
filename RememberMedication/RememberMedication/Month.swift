6a1273bbd97d23147d2a966fd3a71161a78a9672
Alexandre <xan.marconi@hotmail.com>
2016-05-17 15:06:51 -0300

    month color

--- RememberMedication/RememberMedication/Month.swift
+++ RememberMedication/RememberMedication/Month.swift
@@ -58,30 +58,29 @@ extension Month: CustomStringConvertible {
     var color: UIColor {
         switch self {
         case .January:
-            return UIColor.blueColor()
+            return UIColor(red: 249.0 / 255.0, green: 181.0 / 255.0, blue: 178.0 / 255.0, alpha: 1.0)
         case .February:
-            return UIColor.lightGrayColor()
+            return UIColor(red: 234.0 / 255.0, green: 148.0 / 255.0, blue: 215.0 / 255.0, alpha: 1.0)
         case .March:
-            return UIColor.grayColor()
+            return UIColor(red: 243.0 / 255.0, green: 110.0 / 255.0, blue: 75.0 / 255.0, alpha: 1.0)
         case .April:
-            return UIColor.redColor()
+            return UIColor(red: 246.0 / 255.0, green: 148.0 / 255.0, blue: 49.0 / 255.0, alpha: 1.0)
         case .May:
-            return UIColor.blackColor()
+            return UIColor(red: 241.0 / 255.0, green: 221.0 / 255.0, blue: 64.0 / 255.0, alpha: 1.0)
         case .June:
-            return UIColor.magentaColor()
+            return UIColor(red: 198.0 / 255.0, green: 199.0 / 255.0, blue: 82.0 / 255.0, alpha: 1.0)
         case .July:
-            return UIColor.cyanColor()
+            return UIColor(red: 177.0 / 255.0, green: 204.0 / 255.0, blue: 159.0 / 255.0, alpha: 1.0)
         case .August:
-            return UIColor.brownColor()
+            return UIColor(red: 165.0 / 255.0, green: 218.0 / 255.0, blue: 210.0 / 255.0, alpha: 1.0)
         case .September:
-            return UIColor.purpleColor()
+            return UIColor(red: 165.0 / 255.0, green: 188.0 / 255.0, blue: 218.0 / 255.0, alpha: 1.0)
         case .October:
-            return UIColor.greenColor()
+            return UIColor(red: 162.0 / 255.0, green: 161.0 / 255.0, blue: 143.0 / 255.0, alpha: 1.0)
         case .November:
-            return UIColor.orangeColor()
+            return UIColor(red: 200.0 / 255.0, green: 179.0 / 255.0, blue: 132.0 / 255.0, alpha: 1.0)
         case .December:
-            return UIColor.yellowColor()
-            
+            return UIColor(red: 152.0 / 255.0, green: 135.0 / 255.0, blue: 98.0 / 255.0, alpha: 1.0)
         }
     }
 }
